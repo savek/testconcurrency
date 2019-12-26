00e116ef368d31e6f0d77c3cb279334678742960
av.sorokin <A.Sorokin@mils-s.ru>
2019-12-26 10:17:19 +0500

    Очищен метод main

--- src/main/java/com/tack/concurrency/Main.java
+++ src/main/java/com/tack/concurrency/Main.java
@@ -6,14 +6,5 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 
 public class Main {
-	public static void main(String []args) throws Exception {
-
-		Callable task = () -> {
-			return "Hello, World!";
-		};
-
-		FutureTask<String> future = new FutureTask<>(task);
-		new Thread(future).start();
-		System.out.println(future.get());
-	}
+	public static void main(String []args) throws Exception {}
 }
